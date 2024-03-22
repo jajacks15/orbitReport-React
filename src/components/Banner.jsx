b216461faf0b5768ed49f62d3f7e154eb54a3c20
Julian Jackson <julian.jack95@gmail.com>
2024-03-21 21:40:51 -0400

    Graded Assignment 6 final

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import "./styling.css"
+
+ function Banner () {
+
+    return (<div>
+        <header>
+       <h1>Orbit Report</h1> 
+    </header>
+    <p>Click the buttons below to see the satellites in that orbit type.</p>
+        </div>);
+};
+
+export default Banner;
